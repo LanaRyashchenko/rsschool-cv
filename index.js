347c2ba08c9aca7f31ba74ce26a3e3c3bd3f8f47
LanaRyashchenko <siliel.ua@gmail.com>
2022-10-20 14:03:11 +0200

    fix: anchors

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const menuIcon = document.querySelector(".burger");
 const menu = document.querySelector(".menu");
+const link = document.querySelectorAll(".top-menu__link");
 
 menuIcon.addEventListener("click", () => { 
     menu.classList.toggle("menu_open");
+   
 });
+
+for (let i=0; i < link.length; i++) {
+    let item = link[i];
+    item.addEventListener("click", () => { 
+        menu.classList.remove("menu_open");
+    });
+}
+
