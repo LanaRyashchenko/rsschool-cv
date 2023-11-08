b193d4bc7dd2376e2b6839b34752d272f018dc18
LanaRyashchenko <siliel.ua@gmail.com>
2023-11-08 08:44:19 +0100

    fix: typo

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const menuIcon = document.querySelector(".burger");
 const menu = document.querySelector(".menu");
 const link = document.querySelectorAll(".top-menu__link");
 
-menuIcon.addEventListener("click", () => { 
+menuIcon.addEventListener("click", () => {
     menu.classList.toggle("menu_open");
-   
+
 });
 
 for (let i=0; i < link.length; i++) {
     let item = link[i];
-    item.addEventListener("click", () => { 
+    item.addEventListener("click", () => {
         menu.classList.remove("menu_open");
     });
 }
